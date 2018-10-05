12a0610d1879c6be2fc8c0b78e931ea1a392d42f
alexkayabula <31479458+alexkayabula@users.noreply.github.com>
2018-10-05 12:44:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app
 from app.database.database import *
 
 app = create_app('DEFAULT')
-db = Database('postgres://eyzxekqeckzgjc:78e620ab085ae62514bd49db48ce33ec3c50df764c5822e6d8ec9d7607527534@ec2-23-23-80-20.compute-1.amazonaws.com:5432/dac1le4vb3d4of')
+db = Database('postgresql://postgres:k0779211758aj@localhost:5432/order_db')
 
 if __name__ == '__main__':
     
