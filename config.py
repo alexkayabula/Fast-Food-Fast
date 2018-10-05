fad2981a8860b5e07593745be555942fa0a6d1a2
alexkayabula <31479458+alexkayabula@users.noreply.github.com>
2018-10-05 12:44:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Config(object):
     DEBUG = False
     SECRET = os.getenv("SECRET")
     # DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASE_URL = 'postgres://eyzxekqeckzgjc:78e620ab085ae62514bd49db48ce33ec3c50df764c5822e6d8ec9d7607527534@ec2-23-23-80-20.compute-1.amazonaws.com:5432/dac1le4vb3d4of
-'
+    DATABASE_URL = 'postgresql://postgres:k0779211758aj@localhost:5432/order_db'
 
 
 class DevelopmentConfiguration(Config):
@@ -24,12 +23,12 @@ class TestingConfiguration(Config):
 
 class ProductionConfiguration(Config):
     """Configurations for Production."""
-    DEBUG = True
+    DEBUG = False
     DATABASE_URL = 'postgres://eyzxekqeckzgjc:78e620ab085ae62514bd49db48ce33ec3c50df764c5822e6d8ec9d7607527534@ec2-23-23-80-20.compute-1.amazonaws.com:5432/dac1le4vb3d4of'
 
 
 app_config = {
-    'DEFAULT':ProductionConfiguration ,
+    'DEFAULT': DevelopmentConfiguration,
     'TESTING': TestingConfiguration,
     'DEVELOPMENT': DevelopmentConfiguration,
     'PRODUCTION': ProductionConfiguration
