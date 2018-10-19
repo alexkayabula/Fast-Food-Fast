551daac3c7fc8b1be8c748e0ecef2f7788d84194
alexkayabula <31479458+alexkayabula@users.noreply.github.com>
2018-10-19 18:15:45 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import psycopg2
 from app import create_app
 from app.database.database import *
 
-app = create_app('DEFAULT')
-db = Database('postgresql://postgres:k0779211758aj@localhost:5432/order_db')
+app = create_app('PRODUCTION')
+db = Database('postgres://xarplzfllomlvr:b52cdb7c12c1d0175f7569876e8ceaae7d36a3387b93b876f58c67839c95b9a2@ec2-107-22-175-33.compute-1.amazonaws.com:5432/d498qmhc8lmfvk')
 
 if __name__ == '__main__':
     
