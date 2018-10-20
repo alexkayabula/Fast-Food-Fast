b4523c68b91c09f5ed0900c90ebde52f4fd62967
Kayabula Alex Joseph <alexkayabula@gmail.com>
2018-10-20 12:32:24 +0300

    Updates

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ app = create_app('PRODUCTION')
 db = Database('postgres://xarplzfllomlvr:b52cdb7c12c1d0175f7569876e8ceaae7d36a3387b93b876f58c67839c95b9a2@ec2-107-22-175-33.compute-1.amazonaws.com:5432/d498qmhc8lmfvk')
 
 if __name__ == '__main__':
-    db.create_database('order_db')
+    #db.create_database('order_db')
     db.create_tables()
-    port = int(os.environ.get('PORT', 80))
+    port = int(os.environ.get('PORT', 8000))
     app.run(port=port)
